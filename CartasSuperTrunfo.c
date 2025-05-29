08c4f3fcb9ebd97ebe36e526c03a0e31f9475b43
Kevinero <110744879+Kevinero@users.noreply.github.com>
2025-05-29 18:18:37 +0000

    Desafio Aventureiro da logica de programação

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,9 +3,10 @@
 int main() {
     char estado, estado2; // Variaveis do codigo
     char codcarta[5], codcarta2[5], nomecidade[30], nomecidade2[30];
-    int pontosturisticos, pontosturisticos2;
+    int pontosturisticos, pontosturisticos2, opcao;
     float area, area2, pib, pib2, densidade, densidade2, pibcapita, pibcapita2; 
-    unsigned long int populacao, populacao2, ; 
+    unsigned long int populacao, populacao2; 
+
 
     // Coletando os dados da primeira carta
     printf("**BEM VINDO AO JOGO SUPER TRUNFO***\nVamos cadastrar a primeira carta!\nDigite uma letra de 'A' até 'H':\n");
@@ -19,7 +20,7 @@ int main() {
     fgets(nomecidade, 30, stdin);
 
     printf("Digite o número da população:\n");
-    scanf(" %d", &populacao);
+    scanf(" %lu", &populacao);
 
     printf("Digite a área: km2\n");
     scanf(" %f", &area);
@@ -42,7 +43,7 @@ int main() {
     fgets(nomecidade2, 30, stdin);
 
     printf("Digite o número da população:\n");
-    scanf(" %d", &populacao2);
+    scanf(" %lu", &populacao2);
 
     printf("Digite a área: km2\n");
     scanf(" %f", &area2);
@@ -65,15 +66,65 @@ int main() {
     densidade = 1 / densidade;
     densidade2 = 1 / densidade2;
     
-    //Nivel novato na logica de programação: incluindo a logica no programa A comparação será feita pelo pib
-    printf("Comparação das cartas Atributo (PIB)\n");
-    printf("Carta 01 -%s:%.2f\n", nomecidade, pib);
-    printf("Carta 02 -%s:%.2f \n", nomecidade2, pib2);
-    if (pib > pib2) {
-        printf("Resultado: Carta 1 (%s) Venceu!!\n", nomecidade);
-    } else {
-        printf("Resultado: Carta 02 (%s) Venceu!!\n", nomecidade2);
-    }
+        // Escolha de comparação
+    printf("Escolha qual atributo será comparado:\n");
+    printf("1. População\n");
+    printf("2. Pontos Turísticos\n");
+    printf("3. PIB\n");
+    printf("4. Densidade\n");
+    printf("5. PIB per capita\n");
+    scanf("%d", &opcao);
+    
+    printf("Carta 1: %s VS Carta 2: %s", nomecidade, nomecidade2);
 
-    return 0;
-}
+    switch (opcao) {
+        case 1:
+            printf("Carta 1: %lu VS Carta 2: %lu\n", populacao, populacao2);
+            if (populacao > populacao2)
+                printf("Carta 1 venceu!\n");
+            else if (populacao < populacao2)
+                printf("Carta 2 venceu!\n");
+            else
+                printf("Empate!\n");
+            break;
+            case 2:
+            printf("Carta 1: %d VS Carta 2: %d\n", pontosturisticos, pontosturisticos2);
+            if (pontosturisticos > pontosturisticos2)
+                printf("Carta 1 venceu!\n");
+            else if (pontosturisticos < pontosturisticos2)
+                printf("Carta 2 venceu!\n");
+            else
+                printf("Resutado deu Empate!\n");
+            break;
+            case 3:
+            printf("Carta 1: %.2f VS Carta 2: %.2f\n", pib, pib2);
+            if (pib > pib2)
+                printf("Carta 1 venceu!\n");
+            else if (pib < pib2)
+                printf("Carta 2 venceu!\n");
+            else
+                printf("Resutado deu Empate!\n");
+            break;
+            case 4:
+            printf("Carta 1: %.2f VS Carta 2: %.2f\n", densidade, densidade2);
+            if (densidade > densidade2)
+                printf("Carta 2 venceu!\n");
+            else if (densidade < densidade2)
+                printf("Carta 1 venceu!\n");
+            else
+                printf("Resutado deu Empate!\n");
+            break;
+            case 5:
+            printf("Carta 1: %.2f VS Carta 2: %.2f\n", pibcapita, pibcapita2);
+            if (pibcapita > pibcapita2)
+                printf("Carta 1 venceu!\n");
+            else if (pibcapita < pibcapita2)
+                printf("Carta 2 venceu!\n");
+            else
+                printf("Resutado deu Empate!\n");
+            break;
+            default:
+            printf("Opção invalida tente novamente!");
+            break;
+    }
+  }
